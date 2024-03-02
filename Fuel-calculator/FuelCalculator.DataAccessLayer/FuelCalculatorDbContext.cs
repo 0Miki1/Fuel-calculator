3246b24b3b609a88b85c099358c39a7bdd9e7010
Ábel Miklós <mikibalazs04@gmail.com>
2024-03-02 21:34:47 +0100

    Basic data access layer setup

---
+++ Fuel-calculator/FuelCalculator.DataAccessLayer/FuelCalculatorDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FuelCalculator.DataAccessLayer
+{
+    public class FuelCalculatorDbContext : DbContext
+    {
+        public FuelCalculatorDbContext(DbContextOptions<FuelCalculatorDbContext> options) : base(options) { }
+    }
+}
