f55725d4c5526b9b0fa689fc918e6d2cd3784d08
0Miki1 <84203468+0Miki1@users.noreply.github.com>
2024-03-04 23:13:19 +0100

    Created basic architecture
    * Created basic architecture

--- Fuel-calculator/FuelCalculator.DataAccessLayer/FuelCalculatorDbContext.cs
+++ Fuel-calculator/FuelCalculator.DataAccessLayer/FuelCalculatorDbContext.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FuelCalculator.Domain.Entities;
+
+using Microsoft.EntityFrameworkCore;
 
 namespace FuelCalculator.DataAccessLayer
 {
     public class FuelCalculatorDbContext : DbContext
     {
-        public FuelCalculatorDbContext(DbContextOptions<FuelCalculatorDbContext> options) : base(options) { }
+        public FuelCalculatorDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
     }
 }
