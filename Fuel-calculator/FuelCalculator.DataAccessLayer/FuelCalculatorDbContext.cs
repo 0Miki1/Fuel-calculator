62c6d1fc22ec070542d3ff430d5b40c581268823
Ábel Miklós <mikibalazs04@gmail.com>
2024-03-09 00:21:34 +0100

    dotnet format

--- Fuel-calculator/FuelCalculator.DataAccessLayer/FuelCalculatorDbContext.cs
+++ Fuel-calculator/FuelCalculator.DataAccessLayer/FuelCalculatorDbContext.cs
@@ -1,13 +1,13 @@
-﻿using FuelCalculator.Domain.Entities;
+using FuelCalculator.Domain.Entities;
 
 using Microsoft.EntityFrameworkCore;
 
 namespace FuelCalculator.DataAccessLayer
 {
-    public class FuelCalculatorDbContext : DbContext
-    {
-        public FuelCalculatorDbContext(DbContextOptions options) : base(options) { }
+	public class FuelCalculatorDbContext : DbContext
+	{
+		public FuelCalculatorDbContext(DbContextOptions options) : base(options) { }
 
-        public DbSet<User> Users { get; set; }
-    }
+		public DbSet<User> Users { get; set; }
+	}
 }
--- Fuel-calculator/FuelCalculator.Domain/Entities/User.cs
+++ Fuel-calculator/FuelCalculator.Domain/Entities/User.cs
@@ -1,19 +1,19 @@
-﻿#nullable enable
+#nullable enable
 
 using System.ComponentModel.DataAnnotations;
 
 namespace FuelCalculator.Domain.Entities
 {
-    public class User
-    {
-        [Key]
-        public int Id { get; set; }
+	public class User
+	{
+		[Key]
+		public int Id { get; set; }
 
-        [Required, MaxLength(120)]
-        public string UserName { get; set; } = string.Empty;
+		[Required, MaxLength(120)]
+		public string UserName { get; set; } = string.Empty;
 
-        //asdd
-        [Required, MaxLength(60)]
-        public string Password { get; set; } = string.Empty;
-    }
+		//asdd
+		[Required, MaxLength(60)]
+		public string Password { get; set; } = string.Empty;
+	}
 }
