486bd1c30cd5fcbd92f8fddb25061956b6703b53
0Miki1 <84203468+0Miki1@users.noreply.github.com>
2024-03-09 00:29:50 +0100

    Finalize build-validation pipeline

--- Fuel-calculator/FuelCalculator.Domain/Entities/User.cs
+++ Fuel-calculator/FuelCalculator.Domain/Entities/User.cs
@@ -1,18 +1,18 @@
-﻿#nullable enable
+#nullable enable
 
 using System.ComponentModel.DataAnnotations;
 
 namespace FuelCalculator.Domain.Entities
 {
-    public class User
-    {
-        [Key]
-        public int Id { get; set; }
+	public class User
+	{
+		[Key]
+		public int Id { get; set; }
 
-        [Required, MaxLength(120)]
-        public string UserName { get; set; } = string.Empty;
+		[Required, MaxLength(120)]
+		public string UserName { get; set; } = string.Empty;
 
-        [Required, MaxLength(60)]
-        public string Password { get; set; } = string.Empty;
-    }
+		[Required, MaxLength(60)]
+		public string Password { get; set; } = string.Empty;
+	}
 }
